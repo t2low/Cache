cbc0e78ceb183a1becc27cde20b2c4e6d6cbe577
Tetsuro Nakamura <t2low.nk@gmail.com>
2020-02-05 14:15:31 +0900

    CacheがなければFetchするクラスを作成

---
+++ app/src/main/java/com/tappli/cachesample/library/cache/FetchableCache.kt
@@ -0,0 +1,19 @@
+package com.tappli.cachesample.library.cache
+
+import com.tappli.cachesample.library.source.Source
+
+class FetchableCache<KEY : Any, VALUE : Any>(
+    private val source: Source<KEY, VALUE>,
+    private val cache: Cache<KEY, VALUE>
+) : Cache<KEY, VALUE> {
+
+    override suspend fun read(key: KEY): VALUE? {
+        return cache.read(key) ?: source.get(key).also {
+            cache.write(key, it)
+        }
+    }
+
+    override suspend fun write(key: KEY, value: VALUE?) {
+        cache.write(key, value)
+    }
+}
