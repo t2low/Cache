0ceca14d3b1e888fb321d852f0d9d1daed579641
Tetsuro Nakamura <t2low.nk@gmail.com>
2020-02-07 18:10:23 +0900

    UserList表示用Activity追加

---
+++ app/src/main/java/com/tappli/cachesample/presentation/UserListActivity.kt
@@ -0,0 +1,12 @@
+package com.tappli.cachesample.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tappli.cachesample.R
+
+class UserListActivity : AppCompatActivity(R.layout.activity_user_list) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
