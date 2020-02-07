36113ee354ada9b3b7ccb459f97a06bd19894501
Tetsuro Nakamura <t2low.nk@gmail.com>
2020-02-07 17:52:06 +0900

    Koin導入

---
+++ app/src/main/java/com/tappli/cachesample/CacheApplication.kt
@@ -0,0 +1,42 @@
+package com.tappli.cachesample
+
+import android.app.Application
+import com.tappli.cachesample.data.user.repository.DetailUserRepositoryImpl
+import com.tappli.cachesample.data.user.repository.UserListRepositoryImpl
+import com.tappli.cachesample.domain.user.model.UserId
+import com.tappli.cachesample.domain.user.repository.DetailUserRepository
+import com.tappli.cachesample.domain.user.repository.UserListRepository
+import com.tappli.cachesample.domain.user.usecase.*
+import com.tappli.cachesample.presentation.DetailUserViewModel
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+
+class CacheApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@CacheApplication)
+            modules(
+                listOf(
+                    module {
+                        single<DetailUserRepository> { DetailUserRepositoryImpl() }
+                        single<UserListRepository> { UserListRepositoryImpl() }
+                    },
+                    module {
+                        single<GetDetailUserFlowUseCase> { GetDetailUserFlowUseCaseImpl(get()) }
+                        single<UpdateDetailUserUseCase> { UpdateDetailUserUseCaseImpl(get()) }
+                        single<GetUserListFlowUseCase> { GetUserListFlowUseCaseImpl(get()) }
+                        single<UpdateUserListUseCase> { UpdateUserListUseCaseImpl(get()) }
+                    },
+                    module {
+                        viewModel { (userId: UserId) -> DetailUserViewModel(get(), userId, get(), get()) }
+                    }
+                )
+            )
+        }
+    }
+}
--- app/src/main/java/com/tappli/cachesample/presentation/DetailUserActivity.kt
+++ app/src/main/java/com/tappli/cachesample/presentation/DetailUserActivity.kt
@@ -4,23 +4,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import com.tappli.cachesample.R
-import com.tappli.cachesample.data.user.repository.DetailUserRepositoryImpl
 import com.tappli.cachesample.domain.user.model.UserId
-import com.tappli.cachesample.domain.user.usecase.GetDetailUserFlowUseCaseImpl
-import com.tappli.cachesample.domain.user.usecase.UpdateDetailUserUseCaseImpl
 import com.tappli.myapplication.presentation.common.LoadState
 import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
 
 class DetailUserActivity : AppCompatActivity(R.layout.activity_main) {
 
-    private val detailUserViewModel by lazy {
-        DetailUserViewModel(
-            application,
-            UserId(1),
-            GetDetailUserFlowUseCaseImpl(DetailUserRepositoryImpl()),
-            UpdateDetailUserUseCaseImpl(DetailUserRepositoryImpl())
-        )
-    }
+    private val detailUserViewModel by viewModel<DetailUserViewModel> { parametersOf(UserId(1)) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
