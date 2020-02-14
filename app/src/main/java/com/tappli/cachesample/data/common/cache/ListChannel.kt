928a6529786e50a252736196883cdaa63925f69f
Tetsuro Nakamura <t2low.nk@gmail.com>
2020-02-14 20:15:16 +0900

    ChannelとFlowAccessor両方を実装したクラスをListChannelから生成できるようにする

--- app/src/main/java/com/tappli/cachesample/data/common/cache/ListChannel.kt
+++ app/src/main/java/com/tappli/cachesample/data/common/cache/ListChannel.kt
@@ -1,15 +1,39 @@
 package com.tappli.cachesample.data.common.cache
 
 import com.tappli.cachesample.library.cache.Cache
+import com.tappli.cachesample.library.flow.FlowAccessor
 import kotlinx.coroutines.channels.ConflatedBroadcastChannel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.mapNotNull
 
-abstract class ListChannelCache<KEY : Any, VALUE : Any>(private val getKeyFunc: (VALUE) -> KEY) {
+interface FlowAccessorChannel<KEY : Any, VALUE : Any> : Cache<KEY, VALUE>, FlowAccessor<KEY, VALUE>
+
+open class ListChannel<KEY : Any, VALUE : Any>(private val getKeyFunc: (VALUE) -> KEY) {
 
     private val idListChannel = ConflatedBroadcastChannel<List<KEY>>(emptyList())
     private val valueCache = mutableMapOf<KEY, VALUE>()
+
+    fun getValueChannelCache(): FlowAccessorChannel<KEY, VALUE> {
+        return object : FlowAccessorChannel<KEY, VALUE> {
+            override suspend fun read(key: KEY): VALUE? {
+                return valueCache[key]
+            }
+
+            override suspend fun write(key: KEY, value: VALUE?) {
+                valueCache[key]
+                if (idListChannel.value.contains(key)) {
+                    idListChannel.send(idListChannel.value) // NOTE: IDリストの更新をすることで全体に通知
+                }
+            }
+
+            override suspend fun getFlow(key: KEY): Flow<VALUE> {
+                return idListChannel
+                    .asFlow()
+                    .mapNotNull { valueCache[key] }
+            }
+        }
+    }
 
     suspend fun add(values: List<VALUE>) {
         val newIdList = (idListChannel.value + values.map(getKeyFunc)).distinct()
@@ -29,30 +53,9 @@ abstract class ListChannelCache<KEY : Any, VALUE : Any>(private val getKeyFunc: 
             .mapNotNull { valueCache[it] }
     }
 
-    suspend fun getValueCache(): Cache<KEY, VALUE> {
-        return object : Cache<KEY, VALUE> {
-            override suspend fun read(key: KEY): VALUE? {
-                return valueCache[key]
-            }
-
-            override suspend fun write(key: KEY, value: VALUE?) {
-                valueCache[key]
-                if (idListChannel.value.contains(key)) {
-                    idListChannel.send(idListChannel.value) // NOTE: IDリストの更新をすることで全体に通知
-                }
-            }
-        }
-    }
-
     fun getListFlow(): Flow<List<VALUE>> {
         return idListChannel
             .asFlow()
             .mapNotNull { it.mapNotNull { key -> valueCache[key] } }
-    }
-
-    fun getValueFlow(key: KEY): Flow<VALUE> {
-        return idListChannel
-            .asFlow()
-            .mapNotNull { valueCache[key] }
     }
 }
